3462d4995125e555279568d699948c52dbd804f9
Fola Flor <dpcalfola@gmail.com>
2022-04-22 22:34:38 +0900

    Copy files from discarded project
    And install bootstrap

--- config/settings.py
+++ config/settings.py
@@ -124,6 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    BASE_DIR / 'node_modules/bootstrap'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
