cd25e030a854fc9568710a7729b7d53950d4c0d2
Fola Flor <dpcalfola@gmail.com>
2022-04-25 23:23:15 +0900

    Feat > Drink price infomation sharing table
    1. price_table.html
    	Display price data to a table
    
    2. DrinkPriceInformation Model
    	Add note CharField
    
    3. Base html template
    	Code {% block specific_css %} added
    	<head> 에 개별 css 파일 설정 가능

--- market_price/models.py
+++ market_price/models.py
@@ -6,16 +6,22 @@ class DrinkPriceInformation(models.Model):
     주종 (varchar 30 blank=True 위스키, 보드카, 럼...
     이름 (varchar 50 글렌피딕, 아드벡, 아마레또...
     라인업 (varchar 50, blank=True,  그린라벨, Signet, 나두라..
-    숙성년도 (Integer blank=True, -> 12년...
+    숙성년도 (Integer blank=True, null=True(없을시 에러 발생) -> 12년...
     가격 (Integer -> ₩50,000...) -> 모델은 정수 타입. 데이터를 불러온 뒤에 ₩50,000 형태로 파싱
     용량 (Integer -> 700ml)
     구입 경로 (varchar 100)
     가격 확인 날짜 (Date)
+    비고 (varchar 100)
 
     # 여기서부터는 DB 에만 저장
     작성일(DateTime)
     작성자(User) -> 외래키, 타입 방법 확인 요망
     """
+
+    # blank and null rule
+    # CharField -> blank=True
+    # Integer -> blank=True, null=True
+
     category = models.CharField(max_length=30, blank=True)
     name = models.CharField(max_length=50)
     lineup = models.CharField(max_length=50, blank=True)
@@ -24,8 +30,10 @@ class DrinkPriceInformation(models.Model):
     size = models.PositiveIntegerField()
     purchasing_route = models.CharField(max_length=100)
     confirmation_date = models.DateField()
-    # Do not show to web page
-    recoding_date = models.DateTimeField(auto_now=True)
+    note = models.CharField(max_length=100, blank=True)
+
+    # From here - Do not show to web page
+    recoded_date = models.DateTimeField(auto_now=True)
 
     # Add author field later as ForeignKey
     # author = models.ForeignKey()
--- market_price/views.py
+++ market_price/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import DrinkPriceInformation
 
 
 def price_table(request):
-    return render(request, 'market_price/price_table.html')
+    price_list = DrinkPriceInformation.objects.order_by('-confirmation_date')
+
+    context = {
+        'price_list': price_list,
+    }
+
+    return render(request, 'market_price/price_table.html', context)
