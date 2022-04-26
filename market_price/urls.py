7197af9cdbae18daf8dd30eda6959ea5a45d38dc
Fola Flor <dpcalfola@gmail.com>
2022-04-27 01:05:27 +0900

    BugFix > null string input Value Error at Integer field
    The cause of the error is that Django tries to insert null-string value at Integer type field in DB.
    
    According to StackOverflow,
    If the field is passed in None, It will be translated to Null.
    
    However, my Django seems to read null-string('') but 'None' Why?
    
    ```
    form.aged = request.POST.get('input_aged') if request.POST.get('input_aged') else None
    ```
    
    I fixed the error as this code eventually, but I am not satisfied with this way.

--- market_price/urls.py
+++ market_price/urls.py
@@ -5,4 +5,5 @@ app_name = 'market_price'
 
 urlpatterns = [
     path('', views.price_table, name='price_table'),
+    path('insert-price-info/', views.insert_price_info, name='insert_price_info')
 ]
--- market_price/views.py
+++ market_price/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
 from .models import LiqueurPriceInformation
 
 
@@ -14,3 +15,35 @@ def price_table(request):
     }
 
     return render(request, 'market_price/price_table.html', context)
+
+
+def insert_price_info(request):
+    """
+
+    :param request:
+    :return:
+    """
+
+    if request.method == 'POST':
+        form = LiqueurPriceInformation()
+
+        # Input data from form
+        form.category_1 = request.POST.get('input_category_1')
+        form.category_2 = request.POST.get('input_category_2')
+        form.name = request.POST.get('input_liqueur_name')
+        form.lineup = request.POST.get('input_lineup')
+        form.price = request.POST.get('input_price')
+        form.size = request.POST.get('input_size')
+        form.purchasing_route = request.POST.get('input_purchasing_route')
+        form.confirmation_date = request.POST.get('input_confirmation_date')
+        form.note = request.POST.get('input_note')
+        form.recoded_date = timezone.now()
+
+        # This code is the way to avoid Value Error:
+        # Django tries to insert ''(null string) to Integer field in DB
+        # But I don't know why Django doesn't translate null string to None
+        form.aged = request.POST.get('input_aged') if request.POST.get('input_aged') else None
+
+        form.save()
+
+        return redirect('market_price:price_table')
