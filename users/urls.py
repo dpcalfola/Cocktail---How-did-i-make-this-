0fae70a23a22ccf276e06e77d0bfb16ab37e121f
Fola Flor <dpcalfola@gmail.com>
2022-04-29 15:57:19 +0900

    Feat > user/create.html
    Make sign up form
    	- layout and css style

--- users/urls.py
+++ users/urls.py
@@ -7,6 +7,7 @@ app_name = 'users'
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('register/', views.register_page, name='register'),
+    # path('register/', views.register_page, name='register'),
     path('profile/', views.profile_page, name='profile'),
+    path('create/', views.AccountCreateView.as_view(), name='create')
 ]
--- users/views.py
+++ users/views.py
@@ -1,24 +1,37 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
 from .forms import UserResisterForm
 
+
+def profile_page(request):
+    return render(request, 'users/profile.html')
+
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('main:index')
+    template_name = 'users/create.html'
+
+# DISCARDED
+# register is going to be made in Class based view
 
 # def register_page(request):
 #     form = UserCreationForm()
 #     context = {'form': form}
 #     return render(request, 'users/backup/register.html', context)
 
-def register_page(request):
-    if request.method == "POST":
-        return
-
-    else:
-        form = UserResisterForm()
-        context = {
-            'form': form
-        }
-    return render(request, 'users/register.html', context)
-
-
-def profile_page(request):
-    return render(request, 'users/profile.html')
+# def register_page(request):
+#     if request.method == "POST":
+#         return
+#
+#     else:
+#         form = UserResisterForm()
+#         context = {
+#             'form': form
+#         }
+#     return render(request, 'users/register.html', context)
