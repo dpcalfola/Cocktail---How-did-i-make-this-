cadbc4a4a6958d859936d6f9b3fc8dea4d9dd34a
Fola Flor <dpcalfola@gmail.com>
2022-04-29 14:14:36 +0900

    Feat > profile.html
    Add link to profile.html page, urls, functionBasedView
    
    Rewriting sign up logic now on

--- users/views.py
+++ users/views.py
@@ -1,8 +1,24 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
+from .forms import UserResisterForm
 
+
+# def register_page(request):
+#     form = UserCreationForm()
+#     context = {'form': form}
+#     return render(request, 'users/backup/register.html', context)
 
 def register_page(request):
-    form = UserCreationForm()
-    context = {'form': form}
+    if request.method == "POST":
+        return
+
+    else:
+        form = UserResisterForm()
+        context = {
+            'form': form
+        }
     return render(request, 'users/register.html', context)
+
+
+def profile_page(request):
+    return render(request, 'users/profile.html')
