7e1c1e29dd54e339b6c2f66eddca7f5f9da580fe
Fola Flor <dpcalfola@gmail.com>
2022-05-02 22:30:20 +0900

    Feat > Profile card css style
    minor update:
    	install and setup bootstrap-icons

--- config/settings.py
+++ config/settings.py
@@ -135,7 +135,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
-    BASE_DIR / 'node_modules/bootstrap'
+    BASE_DIR / 'node_modules/bootstrap',
+    BASE_DIR / 'node_modules/bootstrap-icons',
 ]
 
 # Default primary key field type
