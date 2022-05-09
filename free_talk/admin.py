2626f4d50dc9892a2e28bde16b76901798b8c965
Fola Flor <dpcalfola@gmail.com>
2022-05-09 18:08:16 +0900

    Feat > Make free_talk model
    Model
    	Post
    	Comment

--- free_talk/admin.py
+++ free_talk/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post, Comment
+
+
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ['subject']
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
--- free_talk/models.py
+++ free_talk/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    subject = models.CharField(max_length=200)
+    content_text = models.TextField()
+    content_image = models.ImageField(upload_to='free_talk/image/', null=True, blank=True)
+    create_date = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.subject
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content_text = models.TextField()
+    create_date = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.post.subject
