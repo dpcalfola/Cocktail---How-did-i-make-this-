1a148cce09937117d5daadbf07b8798ba11cf01a
Fola Flor <dpcalfola@gmail.com>
2022-05-12 01:08:07 +0900

    Feat > free_talk pagination
    board.html free_talk view : CBV -> FBV
    	class PostListView(ListView)
    	-> def board(request)

--- free_talk/urls.py
+++ free_talk/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'free_talk'
 
 urlpatterns = [
-    path('', views.PostListView.as_view(), name='board'),
+    # path('', views.PostListView.as_view(), name='board'),
+    path('', views.board, name='board')
 ]
--- free_talk/views.py
+++ free_talk/views.py
@@ -1,10 +1,7 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Post, Comment
-
-
-def board(request):
-    return render(request, 'free_talk/board.html')
 
 
 class PostListView(ListView):
@@ -13,3 +10,27 @@ class PostListView(ListView):
     template_name = 'free_talk/board.html'
     paginate_by = 8
 
+
+def board(request):
+
+    # Input parameter
+    # If there's no 'page' parameter input, page value will be 1 as default
+    page = request.GET.get('page', 1)
+
+    # Get all post object
+    # Order : newer post first
+    post_list = Post.objects.order_by('-create_date')
+
+    # Paginator
+    per_page = 15
+    paginator = Paginator(post_list, per_page)
+    page_obj = paginator.get_page(page)
+
+    # Get max page index
+    max_index = len(paginator.page_range)
+
+    context = {
+        'post_page_obj': page_obj,
+        'max_index': max_index,
+    }
+    return render(request, 'free_talk/board.html', context)
