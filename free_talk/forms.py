419ac693463c2eaf3cc4cf34a9b189126193bac7
Fola Flor <dpcalfola@gmail.com>
2022-05-17 16:59:58 +0900

    Feat > Show error message
    free_talk Post object create failed error message
    
    Make universal form error message html
    	/templates/form.errors.html
    
    Add code inside of 'templates/free_talk/create.html'
    	{% include 'form_errors.html' %}

--- free_talk/forms.py
+++ free_talk/forms.py
@@ -6,3 +6,8 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['subject', 'content_text', 'content_image']
+
+        labels = {
+            'subject': '제목',
+            'content_text': '내용',
+        }
